cc19fb16a69537d7a53f423d505aa8266e3ac4f3
Francisco Machado e Costa <frmachadoecosta@gmail.com>
2019-08-12 15:00:56 +0100

    Added tabpanels to UI (#5)

--- ui.R
+++ ui.R
@@ -1,38 +1,40 @@
-shinyUI(fluidPage(theme = shinytheme("flatly"),
+shinyUI(navbarPage('RealROC', theme = shinytheme("flatly"),
                   
-    # Application title
-    titlePanel("RealROC"),
-    
-    # Sidebar  
-    sidebarLayout(
-      sidebarPanel(
-        
-        fileInput('main', 'Input file', multiple = FALSE,
-                  accept =".csv"),
-        
-        checkboxInput("header", "Header", TRUE),
-        
-        
-        radioButtons("sep", "Separator",
-                     choices = c(Comma = ",",
-                                 Semicolon = ";",
-                                 Tab = "\t"),
-                     selected = ","),
-        
-        
-        radioButtons("quote", "Quote",
-                     choices = c(None = "",
-                                 "Double Quote" = '"',
-                                 "Single Quote" = "'"),
-                     selected = '"')
-      ),
-  
+
+    tabPanel(title='1. Import Data',
+             
+        # Sidebar
+        sidebarLayout( 
+          sidebarPanel(
+            
+            fileInput('main', 'Input file', multiple = FALSE,
+                      accept =".csv"),
+            
+            checkboxInput("header", "Header", TRUE),
+            
+            
+            radioButtons("sep", "Separator",
+                         choices = c(Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"),
+                         selected = ","),
+            
+            
+            radioButtons("quote", "Quote",
+                         choices = c(None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"),
+                         selected = '"')
+          ),
       
-      # Main Panel
-      mainPanel(
-        tabsetPanel(
-          tabPanel('Datatable', dataTableOutput('filetable'))
-        )
-      )
-    )
+          
+          # Main Panel
+          mainPanel(
+            tabsetPanel(
+              tabPanel('Datatable', dataTableOutput('filetable'))
+              )
+            ))
+          ),
+    tabPanel(title='2. Classic ROC'
+             )
 ))
